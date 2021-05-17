5e49054faec1d6a7c4a4520dafde69eb02affa2c
Ayooluwa Isaiah <ayisaiah@gmail.com>
2021-05-17 20:13:17 +0100

    Add tests for getNewPath function

--- src/validation_test.go
+++ src/validation_test.go
@@ -278,3 +278,72 @@ func TestReportConflicts(t *testing.T) {
 		)
 	}
 }
+
+func TestGetNewPath(t *testing.T) {
+	type m map[string][]struct {
+		source string
+		index  int
+	}
+	cases := []struct {
+		input  string
+		output string
+		m      m
+	}{
+		{
+			input:  "an_image.png",
+			output: "an_image (2).png",
+			m:      nil,
+		},
+		{
+			input:  "an_image (2).png",
+			output: "an_image (3).png",
+			m:      nil,
+		},
+		{
+			input:  "an_image (4).png",
+			output: "an_image (5).png",
+			m:      nil,
+		},
+		{
+			input:  "an_image (8).png",
+			output: "an_image (12).png",
+			m: m{
+				"an_image (8).png": {
+					{
+						source: "img.png",
+						index:  3,
+					},
+				},
+				"an_image (9).png": {
+					{
+						source: "img-2.png",
+						index:  5,
+					},
+				},
+				"an_image (10).png": {
+					{
+						source: "img-3.png",
+						index:  8,
+					},
+				},
+				"an_image (11).png": {
+					{
+						source: "img-4.png",
+						index:  6,
+					},
+				},
+			},
+		},
+	}
+
+	for _, v := range cases {
+		out := getNewPath(v.input, ".", v.m)
+		if out != v.output {
+			t.Fatalf(
+				"Incorrect output from getNewPath. Want: %s, got %s",
+				v.output,
+				out,
+			)
+		}
+	}
+}
