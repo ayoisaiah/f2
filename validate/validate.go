5ef4e407c62f9ec8ed61745623f13a9f2f6c2dc3
Ayooluwa Isaiah <ayisaiah@gmail.com>
2023-09-07 23:46:45 +0200

    move package doc to separate file

--- validate/validate.go
+++ validate/validate.go
@@ -1,17 +1,3 @@
-// Package validate is used to ensure that the renaming operation cannot result
-// in conflicts before the operation is carried out. It protects against the
-// following scenarios:
-//
-// 1. Overwriting a newly renamed path.
-// 2. Target destination contains forbidden characters (varies based on the operating system).
-// 3. Target destination already exists on the file system (except if
-// --allow-overwrite is specified)
-// 4. Target name exceeds the maximum allowed length (255 characters in windows, and 255 bytes on Linux and macOS).
-// 5. Target destination contains trailing periods in any of the sub paths (Windows only).
-// 6. Target destination is empty.
-//
-// It detects each conflicts and reports them, but it can also automatically fix
-// them according to predefined rules (if -F/--fix-conflicts is specified).
 package validate
 
 import (
