eaa04e64102da43e1aa7b3cdf8ec13f4dadcffb9
Ayooluwa Isaiah <ayisaiah@gmail.com>
2024-01-13 07:45:12 +0100

    add stub for CSV tests

--- find/find_test/find_test.go
+++ find/find_test/find_test.go
@@ -252,3 +252,8 @@ func findTest(t *testing.T, cases []testutil.TestCase) {
 func TestFind(t *testing.T) {
 	findTest(t, testCases)
 }
+
+// TestFindCSV tests file matching with CSV files.
+func TestFindCSV(_ *testing.T) {
+	// TODO: Implement tests once CSV implementation has been revised
+}
