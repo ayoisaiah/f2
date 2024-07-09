28a9278e32faf209d96ffbc06d02f44f63f1a15e
Ayooluwa Isaiah <ayisaiah@gmail.com>
2024-07-09 12:40:24 +0100

    dotfiles should not be regarded as hidden in Windows

--- find/find_windows.go
+++ find/find_windows.go
@@ -10,12 +10,6 @@ import (
 
 // checkIfHidden checks if a file is hidden on Windows.
 func checkIfHidden(filename, baseDir string) (bool, error) {
-	// dotfiles also count as hidden
-	// FIXME: Dot files are not hidden in Windows
-	if filename[0] == dotCharacter {
-		return true, nil
-	}
-
 	absPath, err := filepath.Abs(filepath.Join(baseDir, filename))
 	if err != nil {
 		return false, err
